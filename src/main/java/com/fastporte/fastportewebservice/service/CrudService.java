72a7a8ca0eb01c9a84639cec2a7f5636789173fb
Abel Cierto <u20201b561@upc.edu.pe>
2024-06-08 22:21:25 -0500

    modified new changes

--- src/main/java/com/fastporte/fastportewebservice/service/CrudService.java
+++ src/main/java/com/fastporte/fastportewebservice/service/CrudService.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CrudService<T> {
-    T save(T t) throws Exception; //Registrar o actualizar
+    T save(T t) throws Exception; //Reggister or update
     void delete(Long id) throws Exception; //Eliminar
     List<T> getAll() throws Exception; //Traer el listado de cualquier objeto
     Optional<T> getById(Long id) throws Exception; //   Traer un objeto por su id
